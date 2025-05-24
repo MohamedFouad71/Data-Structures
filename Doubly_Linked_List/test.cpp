4798a2cc89a5b26c49f0221a8d46288bc51f64a7
MohamedFouad71 <mf2976527@gmail.com>
2025-05-24 03:36:28 +0300

    Rearranged files in DoublyLinkedList directory

---
+++ Doubly_Linked_List/test.cpp
@@ -0,0 +1,25 @@
+#include "DoublyLinkedList.h"
+#include <iostream>
+using namespace std;
+
+int main () {
+    DoublyLinkedList<float> list1;
+    list1.display();
+    list1.insertLast(15.12);
+    list1.display();
+    list1.insertFirst(12.151);
+    list1.display();
+    list1.insertLast(10);
+    list1.display();
+    DoublyLinkedList<int> list3;
+    list3.insertAt(0, 12);
+    list3.display();
+    list3.insertAt(1, 14);
+    list3.display();
+    list3.insertAt(1, 13);
+    list3.display();
+    list3.insertLast( 15);
+    list3.display();
+    list3.insertAt(1, 3);
+    list3.display();
+}
